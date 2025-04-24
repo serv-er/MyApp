e013dc5c72376a94cfd591f76200e2fce78d6c16
Sarvesh <baranwalrajesh573@gmail.com>
2025-04-24 15:52:35 +0530

    configured reactnativegesturehandler package

--- android/app/src/main/java/com/myapp/MainActivity.kt
+++ android/app/src/main/java/com/myapp/MainActivity.kt
@@ -1,30 +1,26 @@
 package com.myapp
 
-
 import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.ReactRootView
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import org.devio.rn.splashscreen.SplashScreen 
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView 
 
 class MainActivity : ReactActivity() {
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "MyApp"
+    override fun getMainComponentName(): String = "MyApp"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return object : ReactActivityDelegate(this, mainComponentName) {
+            override fun createRootView(): ReactRootView {
+                return RNGestureHandlerEnabledRootView(this@MainActivity) // 👈 This is the magic line
+            }
+        }
+    }
 }
